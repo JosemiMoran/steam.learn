a8c69c5908cbb1559cb263671f330b00e9e5b4bc
JosemiMoran <josemimoran98@gmail.com>
2023-12-13 18:54:17 +0100

    test conection hono to xata

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Hono } from 'hono'
+import { getXataClient } from './xata'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
-
-app.get('/', (c) => c.text('Hello Hono!'))
+const xata = getXataClient()
+const user = xata.db.user.getAll()
+app.use('*',prettyJSON())
+app.get('/', c => c.text('steam'))
+app.get('/user', async (c) => c.json((await user).map((u: { name: any }) => u.name)))
 
 export default app
