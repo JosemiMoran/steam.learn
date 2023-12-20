b3763dbc6bbfc835667335bd8db6031bdedbae1a
JosemiMoran <josemimoran98@gmail.com>
2023-12-20 14:26:33 +0100

    testing vercel integration

--- api/users.ts
+++ api/users.ts
@@ -6,7 +6,6 @@ const app = new Hono();
 const xata = getXataClient();
 const users = xata.db.user.getAll();
 console.log(users);
-app.use("*", prettyJSON());
 app.get("/", async (c) => c.json(await users));
 app.get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
 export default app;
