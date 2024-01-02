ec1f4498d5320bdc569882d3aca0adf850f5d06d
JosemiMoran <josemimoran98@gmail.com>
2024-01-02 19:40:17 +0100

    Update Astro config to use static output instead of serverless

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
   export default defineConfig({
     integrations: [react()], 
-    output: 'server',
+    output: 'static',
     adapter: vercel({
       speedInsights: {
         enabled: true,
