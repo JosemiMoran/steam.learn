91c83ed0ffd0e85f75f4a6ed5a66a75d55a82da6
JosemiMoran <josemimoran98@gmail.com>
2024-01-02 20:04:05 +0100

    Update Astro config and dependencies

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
+const siteUrl =
+  process.env.VERCEL_ENV === 'production'
+    ? 'https://steam-learn-web.vercel.app/' // 'your.prod.domain.here'
+    : 'http://localhost:3000/'
+
+console.log('siteUrl', siteUrl)
   export default defineConfig({
     integrations: [react()], 
-    output: 'static',
+    output: 'server',
     adapter: vercel({
       speedInsights: {
         enabled: true,
