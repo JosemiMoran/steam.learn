b5ae0aabb10e4c618fdd8ea36a5a05c31f1b857a
JosemiMoran <josemimoran98@gmail.com>
2024-01-02 19:17:10 +0100

    Update dependencies and remove unused files

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify"; 
+import vercel from "@astrojs/vercel"; 
 
   export default defineConfig({
     integrations: [react()], 
     output: 'server',
-    adapter: netlify() 
+    adapter: vercel() 
   });
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,16 +1,16 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import users from "./users";
-import {handle} from 'hono/vercel'
-export const runtime = "edge";
 const hono = new Hono().basePath('/api');
 hono.use("*", prettyJSON());
 const route = hono.route('/users',users)
 
 console.log(hono.all)
+
 export default {
     port: 3000,
     fetch: hono.fetch,
   };
+
 export type AppType = typeof route;
 
