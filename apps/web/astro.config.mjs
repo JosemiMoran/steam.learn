6fe9d1ffae6c28b43dbe4a09be3b3149ba36ec54
JosemiMoran <josemimoran98@gmail.com>
2024-01-05 11:48:44 +0100

    Update Astro configuration and package dependencies

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from '@astrojs/vercel/serverless';
 
-console.log('siteUrl', siteUrl)
-  export default defineConfig({
+export default defineConfig({
     integrations: [react()], 
     output: 'server',
     root: "./",
