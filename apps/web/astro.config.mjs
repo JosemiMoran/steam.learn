30615683ca32cdcffb7310456219096777edce18
JosemiMoran <josemimoran98@gmail.com>
2024-01-08 16:08:27 +0100

    netlify -> vercel

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/static'
 
 export default defineConfig({
     integrations: [react()], 
-    output: 'server',
+    output: 'static',
     root: "./",
-    adapter: netlify(),
+    adapter: vercel(),
   });
