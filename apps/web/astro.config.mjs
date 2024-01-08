48e29f8386f005451edcdaef665584f452d4eb3d
JosemiMoran <josemimoran98@gmail.com>
2024-01-08 16:28:20 +0100

    Failed to parse URL from #/users

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import netlify from '@astrojs/netlify';
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
     integrations: [react()], 
-    output: 'static',
+    output: 'server',
     root: "./",
     adapter: vercel(),
   });
