88219f0217e64eebb7040bdb840c229ae81cb4af
JosemiMoran <josemimoran98@gmail.com>
2024-01-08 23:32:58 +0100

    downgrade bun1.0.15

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import netlify from '@astrojs/netlify';
-import vercel from '@astrojs/vercel/serverless'
+import vercelServerless from '@astrojs/vercel/serverless'
 
 export default defineConfig({
     integrations: [react()], 
     output: 'server',
     root: "./",
-    adapter: vercel(),
+    adapter: vercelServerless(),
   });
