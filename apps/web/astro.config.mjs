32c95b539c3030d6480fb6c7930b4d8aa50c492a
JosemiMoran <josemimoran98@gmail.com>
2024-01-10 18:26:05 +0100

    drop vercel try railway

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import node from "@astrojs/node"
 import vercelServerless from '@astrojs/vercel/serverless'
 
 export default defineConfig({
     integrations: [react()], 
     root: "./",
-    output: 'server',
-    adapter: vercelServerless(),
+    adapter: node({
+      mode: "standalone"
+    }),
   });
