461f355103202209f1de7ec857b5ec9d09b47dd0
JosemiMoran <josemimoran98@gmail.com>
2024-01-10 18:55:45 +0100

    remove vercel

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import node from "@astrojs/node"
-import vercelServerless from '@astrojs/vercel/serverless'
 
 export default defineConfig({
     integrations: [react()], 
